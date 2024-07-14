294d7f05a20b243a0ad8d3abfed9d1042581778f
ViktoriiaNesterenko <viktoriia.nesterenko@jooble.com>
2024-07-14 15:28:43 +0300

    add app 'currency'

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-37^0e4i#-4k*brc@#14x!2c2ce-==c_3pt6kcapd!g5b--%)-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'currency',
 ]
 
 MIDDLEWARE = [
